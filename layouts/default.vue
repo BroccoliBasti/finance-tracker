e7c65d1a7c6a61feaafb3b6f77d01721b030c4a8
Sebastian Renkonen <100607489+BroccoliBasti@users.noreply.github.com>
2024-02-02 21:04:39 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mx-auto max-w-4xl">
+        <AppHeader />
+        <main class="my-10">
+            <slot />
+        </main>
+    </div>
+</template>
+  
+<script setup>
+// Link to Google Fonts
+// useHead({
+//     link: [
+//         {
+//             rel: 'preconnect',
+//             href: 'https://fonts.googleapis.com'
+//         },
+//         {
+//             rel: 'stylesheet',
+//             href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+//             crossorigin: ''
+//         }
+//     ]
+// })
+</script>bg-z
+<style>
+/* Set the font family for the entire site */
+body {
+    font-family: 'Inter' sans-serif;
+}
+
+/* Set the background color for the entire site */
+body {
+    @apply dark:bg-zinc-900 bg-zinc-50 dark:text-zinc-50 text-zinc-900
+}
+</style>
