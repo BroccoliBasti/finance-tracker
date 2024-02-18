f312843398c137216383b72ebfee3576c8ac280b
Sebastian Renkonen <100607489+BroccoliBasti@users.noreply.github.com>
2024-02-18 15:52:21 +0200

    add transaction modal component

--- pages/index.vue
+++ pages/index.vue
@@ -7,41 +7,79 @@
   </section>
 
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="false" />
-    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false" />
-    <Trend color="red" title="Savings" :amount="4000" :last-amount="4100" :loading="false" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Savings" :amount="4000" :last-amount="4100" :loading="isLoading" />
   </section>
 
-  <section>
+  <section class="flex justify-between py-2 px-4 mb-10 bg-sky-100/5 ring-1 ring-zinc-700 rounded-md">
+    <div>
+      <h2 class="text-xl font-extrabold">Transactions</h2>
+      <div class="text-zinc-500 dark:text-zinc-400">
+        You have {{ incomeCount }} {{ incomeCount = 1 ? 'income' : 'incomes' }} and {{ expenseCount }} {{ expenses = 1 ?
+          'expense' : 'expenses' }} this period
+      </div>
+    </div>
+    <div>
+      <TransactionModal v-model="isOpen" />
+      <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
+    </div>
+  </section>
+
+  <section v-if="!isLoading">
     <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
       <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
-      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" />
+      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction"
+        @deleted="refreshTransactions()" />
     </div>
+  </section>
+  <section v-else>
+    <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
   </section>
 </template>
 
 <script setup>
 import { transactionViewOptions } from '~/constants';
-const selectedView = ref(transactionViewOptions[1]);
-
 const supabase = useSupabaseClient();
 
+const selectedView = ref(transactionViewOptions[1]);
 const transactions = ref([]);
+const isLoading = ref(false);
+const isOpen = ref(false);
 
-const { data, pending } = await useAsyncData('transactions', async () => {
-  const { data, error } = await supabase
-    .from('transactions')
-    .select();
+const income = computed(() => transactions.value.filter(t => t.type === 'Income'));
+const expense = computed(() => transactions.value.filter(t => t.type === 'Expense'));
 
-  if (error) return [];
+const incomeCount = computed(() => income.value.length);
+const expenseCount = computed(() => expense.value.length);
 
-  return data;
-});
+const incomeTotal = computed(() => income.value.reduce((sum, transaction) => sum + transaction.amount, 0));
 
-console.log(data);
+const expenseTotal = computed(() => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0));
 
-transactions.value = data.value;
+const fetchTransactions = async () => {
+  isLoading.value = true;
+  try {
+    const { data } = await useAsyncData('transactions', async () => {
+      const { data, error } = await supabase
+        .from('transactions')
+        .select();
+
+      if (error) return [];
+
+      return data;
+    });
+
+    return data.value;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const refreshTransactions = async () => transactions.value = await fetchTransactions();
+await refreshTransactions();
+
 
 const transactionsGroupedByDate = computed(() => {
   let grouped = {};
@@ -59,4 +97,4 @@ const transactionsGroupedByDate = computed(() => {
   return grouped;
 });
 
-</script>
+</script>  
